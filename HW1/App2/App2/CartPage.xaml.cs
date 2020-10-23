e935d1861cb3c905b2f34606f582503eaf298bfe
Qivikavi <61878419+Qivikavi@users.noreply.github.com>
2020-10-24 00:52:08 +1000

    Init

---
+++ HW1/App2/App2/CartPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    public class Good
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public string PicPath { get; set; }
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CartPage : ContentPage
+    {
+        public static SortedDictionary<string, Good> goods = new SortedDictionary<string, Good>();
+        public CartPage()
+        {
+            InitializeComponent();
+            goods_list.ItemsSource = goods.Select((thing) => { return thing.Value; }).ToList();
+        }
+        private void Delete_button(object _sender, EventArgs _e)
+        {
+            var item = (Xamarin.Forms.Button)_sender;
+            var listitem = (from itm in goods
+                            where itm.Value.Name == item.CommandParameter.ToString()
+                            select itm).ToList().First();
+            goods.Remove(listitem.Key);
+
+            var buf = goods.Select((a) => { return a.Value; }).ToList();
+            goods_list.ItemsSource = buf;
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (goods.Count() > 0 && await DisplayAlert("Order", "Are you sure?", "Yes", "No"))
+            {
+                goods.Clear();
+                await DisplayAlert("Oreder", "Ordered", "Ok");
+                await Navigation.PopAsync();
+;            }
+        }
+    }
+}
---
+++ HW1/App2/App2/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> images = new List<string> { "water.jpg", "sprite.jpg", "monster.jpg", "schweppes.jpg" };
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void picker_selected_index_changed(object _sender, EventArgs _e)
+        {
+            desc.Text = (_sender as Picker)?.SelectedItem.ToString();
+            img.Source = images[picker.SelectedIndex];
+        }
+        private void stepper_value_changed(object _sender, EventArgs _e)
+        {
+            amount.Text = (_sender as Stepper)?.Value.ToString();
+        }
+        private async void Order_button_clicked(object sender, EventArgs e)
+        {
+            order.IsEnabled = false;
+            await Navigation.PushAsync(new CartPage());
+            order.IsEnabled = true;
+        }
+        private async void Confirm_button_clicked(object sender, EventArgs e)
+        {
+            CartPage.goods[picker.SelectedItem?.ToString()] = new Good()
+            {
+                Name = picker.SelectedItem?.ToString(),
+                Count = int.Parse(amount.Text),
+                PicPath = images[picker.SelectedIndex].ToString()
+            };
+            await DisplayAlert("Order", "Successful", "Ok");
+        }
+        private void amount_text_changed(object sender, TextChangedEventArgs e)
+        {
+            if (amount.Text.Length == 0 || amount.Text.Length >= 3)
+            {
+                stepper.Value = 0;
+            }
+            else if (amount.Text.Length > 0 && amount.Text.All(char.IsDigit))
+            {
+                stepper.Value = Convert.ToInt32(amount.Text);
+            }
+            else if (amount.Text == "-")
+            {
+                amount.Text = amount.Text.Remove(amount.Text.Length - 1);
+            }
+            if (stepper.Value == 0)
+            {
+                amount.Text = "";
+            }
+        }
+    }
+}
